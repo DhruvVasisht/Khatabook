31f5b5b1fab9f006d35af8c2a9fb8b06b81e3587
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-07-14 17:28:26 +0530

    Complete

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
