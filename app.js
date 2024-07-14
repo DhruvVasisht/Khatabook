ddf0e2949af76f11727637ad2282969e970cc094
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-07-14 18:25:28 +0530

    c

--- app.js
+++ app.js
@@ -1,77 +1,82 @@
-const express = require('express');
+const http = require('http');
+const  express = require('express');
+const app = express();
 const path = require('path');
 const fs = require('fs');
-const app = express();
-
-// Middleware for JSON and URL-encoded data
+const server= http.createServer((req, res) => {
+    if(req.url === '/' || req.url === '/views/index.ejs'){
+        fs.readdir(`./hisaab`,(err, files) =>{
+            if(err) return  res.status(500).send(err);
+            res.render('index', {files: files});
+        })
+    }
+    // else if(req.url === '/create.html'){
+    //     res.render('create');
+    // }
+    // else if(req.url.startsWith('/edit/')){
+    //     const filename = req.url.split('/')[2];
+    //     fs.readFile(`./hisaab/${filename}`,"utf-8", (err, filedata) =>{
+    //         if(err) return  res.status(500).send(err);
+    //         res.render("edit", {filedata, filename});
+    //     })
+    // }
+    // else if(req.url.startsWith('/hisaab/')){
+    //     const filename = req.url.split}
+})
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Setting view engine
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, "public")));
+app.get('/', (req, res) =>{
+    fs.readdir(`./hisaab`,(err, files) =>{
+        if(err) return  res.status(500).send(err);
+        res.render('index', {files: files});
+    })
+   
+})
 
-// Ensure 'hisaab' directory exists
-const hisaabDir = path.join(__dirname, 'hisaab');
-if (!fs.existsSync(hisaabDir)){
-    fs.mkdirSync(hisaabDir);
-}
-
-// Route: Home
-app.get('/', (req, res) => {
-    fs.readdir(hisaabDir, (err, files) => {
-        if (err) return res.status(500).send(err);
-        res.render('index', { files });
-    });
-});
-
-// Route: Create page
-app.get('/create', (req, res) => {
+app.get("/create",(req, res) =>{
     res.render('create');
 });
 
-// Route: Edit page
-app.get('/edit/:filename', (req, res) => {
-    fs.readFile(path.join(hisaabDir, req.params.filename), 'utf-8', (err, filedata) => {
-        if (err) return res.status(500).send(err);
-        res.render('edit', { filedata, filename: req.params.filename });
-    });
+app.get("/edit/:filename",(req, res) =>{
+    fs.readFile(`./hisaab/${req.params.filename}`,"utf-8", (err, filedata) =>{
+        if(err) return  res.status(500).send(err);
+        res.render("edit", {filedata, filename: req.params.filename});
+    })
 });
 
-// Route: View file
-app.get('/hisaab/:filename', (req, res) => {
-    fs.readFile(path.join(hisaabDir, req.params.filename), 'utf-8', (err, filedata) => {
-        if (err) return res.status(500).send(err);
-        res.render('hisaab', { filedata, filename: req.params.filename });
-    });
+app.get("/hisaab/:filename",(req, res) =>{
+    fs.readFile(`./hisaab/${req.params.filename}`,"utf-8", (err, filedata) =>{
+        if(err) return  res.status(500).send(err);
+        res.render("hisaab", {filedata, filename:req.params.filename});
+    })
 });
 
-// Route: Delete file
-app.get('/delete/:filename', (req, res) => {
-    fs.unlink(path.join(hisaabDir, req.params.filename), (err) => {
-        if (err) return res.status(500).send(err);
+app.get("/delete/:filename",(req, res) =>{
+    fs.unlink(`./hisaab/${req.params.filename}`, (err, filedata) =>{
+        if(err) return  res.status(500).send(err);
+        res.redirect("/");
+    })
+});
+
+
+app.post("/update/:filename",(req, res) =>{
+   fs.writeFile(`./hisaab/${req.params.filename}`, req.body.content, (err) =>{
+    if(err) return  res.status(500).send(err);
         res.redirect('/');
-    });
+   })
 });
 
-// Route: Update file
-app.post('/update/:filename', (req, res) => {
-    fs.writeFile(path.join(hisaabDir, req.params.filename), req.body.content, (err) => {
-        if (err) return res.status(500).send(err);
+app.post("/createhisaab",(req, res) =>{
+    var uniqueIdentifier = Date.now();
+    var currentDate= new Date();
+    var date=`${currentDate.getDate()}-${currentDate.getMonth()+1}-${currentDate.getFullYear()}-${currentDate.getHours()}hrs`
+    fs.writeFile(`./hisaab/${date}.txt`,req.body.content,function(err){
+        if(err) return res.status(500).send(err);
         res.redirect('/');
-    });
+    })
 });
-
-// Route: Create new file
-app.post('/createhisaab', (req, res) => {
-    const currentDate = new Date();
-    const date = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}-${currentDate.getHours()}hrs`;
-    const filePath = path.join(hisaabDir, `${date}.txt`);
-    fs.writeFile(filePath, req.body.content, (err) => {
-        if (err) return res.status(500).send(err);
-        res.redirect('/');
-    });
-});
-
-// Export the app for serverless function
-module.exports = app;
+app.listen(3000);
+server.listen( () => console.log(`Server running at http://127.0.0.1:/`));
